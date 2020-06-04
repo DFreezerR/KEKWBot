25706fda00de27e91c1dde20941f45d90c5d13bc
xHappyManx <neomatrix1901@gmail.com>
2020-06-05 01:15:58 +0300

    Added prefix KEKW

--- bot.js
+++ bot.js
@@ -1,23 +1,70 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-var count = 0;
+var working = true;
+
+//var fs = require('fs');
+
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', message => {
-    
-    if (message.author.bot != true) 
+    var prefix = "KEKW ";
+    var input = message.content.substr(0,5);
+    if (input == prefix && message.author.bot != true) 
     {
-      if(message.content == "KEKW Count")
+      var command = message.content.substr(5, message.content.length-1).trim.toLowerCase();
+      switch(command)
       {
-        message.channel.send("'FUCK YOU'ed "+count+" times.");
+        case 'count':
+          {
+            message.channel.send("'FUCK YOU'ed "+count+" times.");
+          } break;
+        case 'switch':
+          {
+            working = !working;
+          } break;
+        default:
+          {
+            if(working)
+            {
+              message.reply("FUCK YOU");
+              count++;
+            }
+          } break;
+      }
+      
+      /*if(command == "KEKW Count")
+      {
+        var jsonData = fs.readFileSync('./config.json');
+        var count = null;
+        try {
+          count = JSON.parse(jsonData);
+          console.log(count);
+          message.channel.send("'FUCK YOU'ed "+count+" times.");*/
+        /*}
+        catch (err) {
+          console.log('There has been an error parsing your JSON.')
+          console.log(err);
+        }
+        
       }
       else
       {
+        var options = {
+          counter: 0
+        };
+        var data = JSON.stringify(options);
+        fs.writeFile('./config.json', data, function (err) {
+          if (err) {
+            console.log('There has been an error saving your configuration data.');
+            console.log(err.message);
+            return;
+          }
+        });
         message.reply("FUCK YOU");
         count++;
-      }
+      }*/
     }
 
 });
