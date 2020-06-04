62492ab9d4f5a352409cef49c7aa3dbd6c498583
xHappyManx <neomatrix1901@gmail.com>
2020-06-05 01:19:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ bot.on('message', message => {
     var input = message.content.substr(0,5);
     if (input == prefix && message.author.bot != true) 
     {
-      var command = message.content.substr(5, message.content.length-1).trim.toLowerCase();
+      var command = message.content.substr(5, message.content.length-1).trim;
       switch(command)
       {
         case 'count':
@@ -26,45 +26,17 @@ bot.on('message', message => {
           } break;
         default:
           {
-            if(working)
+            
+          } break;
+      }
+    }
+    else
+    {
+      if(working)
             {
               message.reply("FUCK YOU");
               count++;
             }
-          } break;
-      }
-      
-      /*if(command == "KEKW Count")
-      {
-        var jsonData = fs.readFileSync('./config.json');
-        var count = null;
-        try {
-          count = JSON.parse(jsonData);
-          console.log(count);
-          message.channel.send("'FUCK YOU'ed "+count+" times.");*/
-        /*}
-        catch (err) {
-          console.log('There has been an error parsing your JSON.')
-          console.log(err);
-        }
-        
-      }
-      else
-      {
-        var options = {
-          counter: 0
-        };
-        var data = JSON.stringify(options);
-        fs.writeFile('./config.json', data, function (err) {
-          if (err) {
-            console.log('There has been an error saving your configuration data.');
-            console.log(err.message);
-            return;
-          }
-        });
-        message.reply("FUCK YOU");
-        count++;
-      }*/
     }
 
 });
