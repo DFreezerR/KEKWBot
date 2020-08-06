f294ec90d3da51a64474d82fd34befbc04d0f058
xHappyManx <neomatrix1901@gmail.com>
2020-08-06 17:08:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -145,11 +145,11 @@ bot.on('message', message => {
       }
       if(utils.random(0,100)<=50)
       {
-        message.react(utils.emotes[utils.random(0, Object.keys(utils.emotes).length-1)]);
+        message.react(utils.emotes[utils.random(0, 3)]);
       }
       if(utils.random(0,100)<=50)
       {
-        message.reply(utils.emotes[utils.random(0, Object.keys(utils.emotes).length-1)]);
+        message.reply(utils.emotes[utils.random(0, 3)]);
       }
     }
 
