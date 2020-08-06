a0887cceb497b415720f7b619999e78c933c2c86
xHappyManx <neomatrix1901@gmail.com>
2020-08-06 17:31:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,6 +153,7 @@ bot.on('message', message => {
         console.log(utils.getRandomEmote());
         console.log(utils.emotes.keys());
         console.log(utils.emotes.key(0));
+        console.log(utils.emotes.key(utils.random(0,2)));
       }
     }
 
