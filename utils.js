946fe606e49738783515f02571bcaf6cae83b77b
xHappyManx <neomatrix1901@gmail.com>
2020-10-21 15:11:09 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -7,10 +7,9 @@ let getImgurId = (length) =>
 {
   let result = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let charactersLength = characters.length;
   for (let i = 0; i < length; i++ ) 
   {
-     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
 }
