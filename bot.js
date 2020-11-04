520b75d043fe3e9344b36ef831c9a81620d414c4
xHappyManx <neomatrix1901@gmail.com>
2020-11-04 17:01:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,22 @@ const client = new Client(dbConfig);
 bot.on('ready', () => 
 {
   console.log(`Logged in as ${bot.user.tag}!`);
-  
+  pool.connect((e,client,done) =>
+  {
+    if (e) return console.error('connection error',e);
+    client.query('SELECT * FROM blacklist_words').then(res =>
+      {
+        for (let row of res.rows) 
+        {
+          banHorny.push(row['word']);
+          
+        }
+      }).catch(e=>
+        {
+          return console.error(e);
+        });
+  });
+  //console.log(banHorny);
 });
 let getImgurImg = (id) =>
 {
