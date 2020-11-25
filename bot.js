219fcf067e85d1fb4f50bed22014fd7083c753c3
xHappyManx <neomatrix1901@gmail.com>
2020-11-25 21:42:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,7 +166,7 @@ bot.on('message', message =>
               {
                 pool.connect().then(client =>
                   {
-                    if((message.content.split("\"")).length - 1 < 3)
+                    if((message.content.split("\"")).length - 1 < 2)
                     {
                       message.reply("Use \" \" to remove your string");
                       return;
