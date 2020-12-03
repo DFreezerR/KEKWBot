04a3b434e2c5afbe1fcc0edf88f2354cb5b3fe1b
xHappyManx <neomatrix1901@gmail.com>
2020-12-03 03:03:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -130,6 +130,7 @@ bot.on('message', message =>
               console.log(buffer);
               Vibrant.from(buffer).getPalette().then((palette) =>
               {
+                console.log(palette);
                 message.channel.send(palette);
               });
             })()
